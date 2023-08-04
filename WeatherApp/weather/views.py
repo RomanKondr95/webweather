cf1fc6a2c50b010fa847f2b418c687de75e2b939
RomanKondr95 <139111155+RomanKondr95@users.noreply.github.com>
2023-08-04 11:55:44 +0300

    добавил возможность посмотреть погоду
    Поправил функцию "index.py" в "views" и "index.html, добавив ссылку на "openweathermaps.org"

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
-
+import requests
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = 'f0267f73b973e2f7cc32b86b7c1f58e6'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = 'Cherepovets'
+    response = requests.get(url.format(city)).json()
+    city_info = {
+        'city': city,
+        'temp': response["main"]["temp"],
+        'icon': response["weather"][0]["icon"]
+    }
+    context = {'info': city_info}
+
+    return render(request, 'weather/index.html',context)
