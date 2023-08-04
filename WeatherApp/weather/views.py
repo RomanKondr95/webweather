ced5d82f8962f9731a53d258aa6bd4ec79ccfcad
RomanKondr95 <139111155+RomanKondr95@users.noreply.github.com>
2023-08-04 14:22:46 +0300

    добавил возможность отображать  погоду из БД
    сделал миграцию в БД названий городов. Поправил функцию "index".
    Теперь все города из базы данных будут отображаться на главной странице

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 import requests
+from .models import City
 def index(request):
     appid = 'f0267f73b973e2f7cc32b86b7c1f58e6'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city = 'Cherepovets'
-    response = requests.get(url.format(city)).json()
-    city_info = {
-        'city': city,
-        'temp': response["main"]["temp"],
-        'icon': response["weather"][0]["icon"]
-    }
-    context = {'info': city_info}
+    # city = 'Cherepovets'
+    cities = City.objects.all()
+    all_cities = []
+    for city in cities:
+        response = requests.get(url.format(city.name)).json()
+        city_info = {
+            'city': city.name,
+            'temp': response["main"]["temp"],
+            'icon': response["weather"][0]["icon"]
+        }
+        all_cities.append(city_info)
+    context = {'all_info': all_cities}
 
     return render(request, 'weather/index.html',context)
